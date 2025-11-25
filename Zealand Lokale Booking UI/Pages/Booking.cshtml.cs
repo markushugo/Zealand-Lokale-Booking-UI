51aeb16c5b6863a470660fc64860875e7e7c6898
markushugo <mal024@edu.zealand.dk>
2025-11-25 10:30:31 +0100

    Created lists for all the different filtering options and made it show the list options on the site in the filtering

--- Zealand Lokale Booking UI/Pages/Booking.cshtml.cs
+++ Zealand Lokale Booking UI/Pages/Booking.cshtml.cs
@@ -5,8 +5,22 @@ namespace Zealand_Lokale_Booking_UI.Pages
 {
     public class BookingModel : PageModel
     {
+        public List<string> Departments { get; set; } = new();
+        public List<string> Buildings { get; set; } = new();
+        public List<string> Floors { get; set; } = new();
+        public List<string> Types { get; set; } = new();
+        public List<string> Rooms { get; set; } = new();
+        public List<string> Times { get; set; } = new();
+
         public void OnGet()
         {
+            Departments = new List<string> { "Roskilde", "Køge", "Slagelse", "Næstved", "Holbæk", "Nykøbing Falster", "Nødebo" };
+            Buildings = new List<string> { "A", "D" };
+            Floors = new List<string> { "1", "2", "3" };
+            Rooms = new List<string> { "1", "2" };
+            Times = new List<string> { "08-10", "10-12", "12-14", "14-16" };
+            Types = new List<string> { "Klasselokale", "Study Room", "Auditorium" };
         }
+
     }
 }

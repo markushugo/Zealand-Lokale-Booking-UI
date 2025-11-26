ac25059135196b7986ce9803320265c3454e2561
markushugo <mal024@edu.zealand.dk>
2025-11-26 10:17:57 +0100

    Outcommented

--- Zealand Lokale Booking UI/Pages/Booking.cshtml.cs
+++ Zealand Lokale Booking UI/Pages/Booking.cshtml.cs
@@ -6,32 +6,32 @@ namespace Zealand_Lokale_Booking_UI.Pages
 {
     public class BookingModel : PageModel
     {
-        //private readonly IBookingService _bookingService;
+        private readonly IBookingService _bookingService;
 
-        //public BookingModel(IBookingService bookingService)
-        //{
-        //    _bookingService = bookingService;
-        //}
-        //public List<RoomBooking> Bookings { get; set; } = new();
+        public BookingModel(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        public List<RoomBooking> Bookings { get; set; } = new();
 
-        //public List<string> Departments { get; set; } = new();
-        //public List<string> Buildings { get; set; } = new();
-        //public List<int> Floors { get; set; } = new();
-        //public List<string> Types { get; set; } = new();
-        //public List<string> Rooms { get; set; } = new();
-        //public List<string> Times { get; set; } = new();
+        public List<string> Departments { get; set; } = new();
+        public List<string> Buildings { get; set; } = new();
+        public List<int> Floors { get; set; } = new();
+        public List<string> Types { get; set; } = new();
+        public List<string> Rooms { get; set; } = new();
+        public List<string> Times { get; set; } = new();
 
-        //public void OnGet()
-        //{
-        //    // Collects data from SQL
-        //    Departments = _bookingService.GetDepartments().ToList();
-        //    Buildings = _bookingService.GetBuildings().ToList();
-        //    Floors = _bookingService.GetFloors().ToList();
-        //    Types = _bookingService.GetTypes().ToList();
-        //    Rooms = _bookingService.GetRooms().ToList();
-        //    Times = _bookingService.GetTimes().ToList();
+        public void OnGet()
+        {
+            // Collects data from SQL
+            Departments = _bookingService.GetDepartments().ToList();
+            Buildings = _bookingService.GetBuildings().ToList();
+            Floors = _bookingService.GetFloors().ToList();
+            Types = _bookingService.GetTypes().ToList();
+            Rooms = _bookingService.GetRooms().ToList();
+            Times = _bookingService.GetTimes().ToList();
 
-        //    Bookings = _bookingService.GetBookings(new BookingFilter()).ToList();
-        //}
+            Bookings = _bookingService.GetBookings(new BookingFilter()).ToList();
+        }
     }
 }

dba2b11f5fcb1ae015003caafcbf00297bfe7bfe
mal025 <mal025@edu.zealand.dk>
2025-11-26 15:18:21 +0100

    Fixed markus's anoying project cloning methode!!!!

--- Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
+++ Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8503ac54ba14beb65ddc78e6b9c902aa97ef73a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f152d5ab7dbc181063fd1d514c7679c154a36fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

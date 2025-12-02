25f886416fb70789e1f5cfb2f58caaad1688d3ac
mal025 <mal025@edu.zealand.dk>
2025-12-02 17:56:15 +0100

    Readded program.cs

---
+++ Zealand Lokale Booking UI/Program.cs
@@ -0,0 +1,63 @@
+﻿using Zealand_Lokale_Booking_Library.Repos;
+using Zealand_Lokale_Booking_Library.Services;
+using Zealand_Lokale_Booking_Library.Repos;
+
+namespace Zealand_Lokale_Booking_UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            string _connectionString =
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ZealandBooking;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;";
+
+            // Razor Pages
+            builder.Services.AddRazorPages();
+
+            // -------------------------------
+            // Register interfaces -> implementations
+            // -------------------------------
+            //builder.Services.AddSingleton<ICreateBookingRepo>(new CreateBookingRepo(_connectionString));
+            //builder.Services.AddSingleton<ICreateBookingService, CreateBookingService>();
+
+            // Session support
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(60);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddScoped<FilterRepository>(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                var connStr = "Server=(localdb)\\MSSQLlocaldb; database=ZealandBooking; encrypt=false; integrated security=true;";
+                return new FilterRepository(connStr);
+            });
+
+            var app = builder.Build();
+
+            // HTTP pipeline
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}

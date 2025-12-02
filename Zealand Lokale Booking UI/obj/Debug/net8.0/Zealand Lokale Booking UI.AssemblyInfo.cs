e5a4147500311fd238106341dcc98e315a636289
Markus Viktor Lynggaard <mal024@edu.zealand.dk>
2025-12-02 11:11:45 +0100

    Create table for mybookings showing

--- Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
+++ Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851a9e88a074ab2202645d1e51bffff9a2d63c32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273e8e046fc3c5e0f73c29bdab135ad9aea22885")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

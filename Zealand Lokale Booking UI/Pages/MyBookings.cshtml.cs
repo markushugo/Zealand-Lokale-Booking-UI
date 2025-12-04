8ee6a563328a73ed19b2d78410a6a63f48e7e6e9
mal025 <mal025@edu.zealand.dk>
2025-12-04 13:51:57 +0100

    Added table view of own bookings, with data from repository

--- Zealand Lokale Booking UI/Pages/MyBookings.cshtml.cs
+++ Zealand Lokale Booking UI/Pages/MyBookings.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealand_Lokale_Booking_Library.Models;
+using Zealand_Lokale_Booking_Library.Repos;
 
 namespace Zealand_Lokale_Booking_UI.Pages
 {
     public class MyBookingsModel : PageModel
     {
-        public void OnGet()
+        private readonly GetBookingsRepo _getBookingsRepo = new GetBookingsRepo("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ZealandBooking;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;");
+
+        public List<Booking> MyBookings { get; set; } = new();
+
+        public async Task OnGetAsync()
         {
+            // TODO: replace with logged-in user ID when authentication is implemented
+            int userId = 1;
+            MyBookings = await _getBookingsRepo.GetBookingsByUserIdAsync(userId);
         }
     }
 }

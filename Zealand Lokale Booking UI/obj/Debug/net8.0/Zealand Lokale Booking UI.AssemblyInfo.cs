80014a8d615d760d59bdb743336d80ffa7a199cc
Markus Viktor Lynggaard <mal024@edu.zealand.dk>
2025-12-09 11:06:03 +0100

    fix

--- Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
+++ Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550c77af5d6505c6248a77a12a31baa6cf6dd0f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e3f3c5233a526aac44a8889017514a97d2a5be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

9b936e2972d5f0ee0d3b332518ef32c40fc785e0
mal025 <mal025@edu.zealand.dk>
2025-12-10 12:36:25 +0100

    Implemented bookingservice in mybookings

--- Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
+++ Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe12baae30f18908c086283b42e5a884770e922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeab64d01cabcac8c2783d72e314fc066d6ea8da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

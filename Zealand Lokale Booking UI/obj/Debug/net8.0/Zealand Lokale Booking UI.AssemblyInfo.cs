2d53394a6f8f12f2e9067d913a182169c9da4f6e
VisualizedConfusion2 <thh005@edu.zealand.dk>
2025-12-17 11:51:26 +0100

    g

--- Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
+++ Zealand Lokale Booking UI/obj/Debug/net8.0/Zealand Lokale Booking UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5734e211e1f5c2210a47bbe331f7091c1990df5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824bb8404633030c2c1b2287bd2cd01167ee6e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zealand Lokale Booking UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
